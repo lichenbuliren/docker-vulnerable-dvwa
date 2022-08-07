05cd3c388d95bdc59db5064c3753c2a9e4629103
rufeng.wei <rufeng.wei@seamoney.com>
2022-08-07 18:48:40 +0800

    feat(modify): add init data

--- config.inc.php
+++ config.inc.php
@@ -26,13 +26,13 @@ $_DVWA[ 'db_port '] = '5432';
 # ReCAPTCHA settings
 #   Used for the 'Insecure CAPTCHA' module
 #   You'll need to generate your own keys at: https://www.google.com/recaptcha/admin/create
-$_DVWA[ 'recaptcha_public_key' ]  = '';
-$_DVWA[ 'recaptcha_private_key' ] = '';
+$_DVWA[ 'recaptcha_public_key' ]  = '6LcFslQhAAAAANishxe8Btju5XEBwunds3CtAhap';
+$_DVWA[ 'recaptcha_private_key' ] = '6LcFslQhAAAAAI7WlqtaYmDYyUIJhTlAkH';
 
 # Default security level
 #   Default value for the secuirty level with each session.
 #   The default is 'impossible'. You may wish to set this to either 'low', 'medium', 'high' or impossible'.
-$_DVWA[ 'default_security_level' ] = 'low';
+$_DVWA[ 'default_security_level' ] = 'impossible';
 
 # Default PHPIDS status
 #   PHPIDS status with each session.
